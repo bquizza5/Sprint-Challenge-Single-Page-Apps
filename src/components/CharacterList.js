b828f7e06c33ba567b18c090b40b64e48246899d
bquizza <blaine555@gmail.com>
2019-07-26 10:17:29 -0600

    characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState()
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(char => {
+        // console.log(char.data.results)
+        setCharacters(char.data.results)
+
+      })
+      .catch(error => {
+        console.log('fetch error: ' + error)
+      })
+
   }, [])
 
-  return <section className='character-list grid-view'>
+  if (!characters) {
+    return <h1>loading</h1>
+  } else {
 
-      <h2>TODO: `array.map()` over your state here!</h2>
+    return <section className='character-list grid-view'>
+
+
+      {
+        // console.log(characters)
+        characters.map((person) => {
+          return (
+            <div className='card'>
+              <img src={person.image}></img>
+              <div className='charData'>
+                <h3>{person.name}</h3>
+                <p>{`${person.species} ${person.status}`}</p>
+                <p>Location: {person.location.name}</p>
+                <p>Location: {person.origin.name}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+
     </section>
 
+  }
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,54 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios'
 
-export default function LocationsList() {
+export default function LocationList() {
+  // TODO: Add useState to track data from useEffect
+  const [locations, setLocations] = useState()
 
+  useEffect(() => {
+    // TODO: Add AJAX/API Request here - must run in `useEffect`
+    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+
+
+    axios
+      .get('https://rickandmortyapi.com/api/location/')
+      .then(loc => {
+        console.log(loc.data.results)
+        setLocations(loc.data.results)
+
+      })
+      .catch(error => {
+        console.log('fetch error: ' + error)
+      })
+
+  }, [])
+
+  if (!locations) {
+    return <h1>loading</h1>
+  } else {
+
+    return <section className='location-list grid-view'>
+
+
+      {
+        //   console.log(locations)
+        
+        // locations.map((location) => {
+        //   return (
+        //     <div className='card'>
+        //       <div className='charData'>
+        //         <h3>{location.name}</h3>
+        //         <p>{`${person.species} ${person.status}`}</p>
+        //         <p>Location: {person.location.name}</p>
+        //         <p>Location: {person.origin.name}</p>
+        //       </div>
+        //     </div>
+        //   )
+        // })
+      }
+
+    </section>
+
+  }
 }
+
